c6b25411feadb8eaad225161b78fd42bf3f6a531
kamil <km@greensteam.com>
2017-10-30 13:34:17 +0100

    initial version of setup scripts

---
+++ basic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# atom repo
+sudo add-apt-repository ppa:webupd8team/atom
+
+# chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+# spotify
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D2C19886
+sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list'
+
+# installation
+sudo apt-get update
+sudo apt-get install -y atom git git-cola mc vim vlc google-chrome-stable zsh curl spotify-client
+
+
+# oh my zsh config
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://github.com/kmuszyn/utils/blob/master/bira-mod.zsh-theme
+mv bira-mod.zsh-theme ~/.oh-my-zsh/themes/
+sed -i -e 's/robbyrussell/bira-mod/g' ~/.zshrc
+
+# git config
+
+git config --global user.name "Kamil Muszynski"
+git config --global user.email muszynski.kamil@gmail.com
---
+++ work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# docker
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# slack
+
+
+sudo apt-get update
+sudo apt-get install -y docker-ce network-manager-openvpn-gnome
+
+# for pycharm etc.
+mkdir ~/bin
+
+
+echo ''
+echo 'Install pycharm from https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=linux'
+git config --global user.email km@greensteam.com
